c778e7fb3cb4b55021fac50c719e4d6e5d9ec615
linbo.jin <linbojin203@gmail.com>
2014-10-25 20:12:33 +0800

    Upload version 1.0
    Implemented by python and matlab

---
+++ thinning.m
@@ -0,0 +1,68 @@
+% @Author:  Linbo<linbo.me>
+% @Version: 1.0  25/10/2014
+% This is the implementation of the 
+% Zhang-Suen Thinning Algorithm for skeletonization.
+
+clc,clear
+% load image data
+addpath('./data')
+Img_Original = imread('test1.bmp');
+
+% Convert gray images to binary images using Otsu's method
+Otsu_Threshold = graythresh(Img_Original);
+BW_Original = not(im2bw(Img_Original,Otsu_Threshold));  % must set object region as 1, background region as 0 
+
+
+changing = 1;
+[rows, columns] = size(BW_Original);
+BW_Thinned = BW_Original;
+BW_Del = ones(rows, columns);
+while changing
+    % BW_Del = ones(rows, columns);
+    changing = 0;
+    % Setp 1
+    for i=2:rows-1
+        for j = 2:columns-1
+            P = [BW_Thinned(i,j) BW_Thinned(i-1,j) BW_Thinned(i-1,j+1) BW_Thinned(i,j+1) BW_Thinned(i+1,j+1) BW_Thinned(i+1,j) BW_Thinned(i+1,j-1) BW_Thinned(i,j-1) BW_Thinned(i-1,j-1) BW_Thinned(i-1,j)]; % P1, P2, P3, ... , P8, P9, P2
+            if (BW_Thinned(i,j) == 1 &&  sum(P(2:end-1))<=6 && sum(P(2:end-1)) >=2 && P(2)*P(4)*P(6)==0 && P(4)*P(6)*P(8)==0)   % conditions
+                % No. of 0,1 patterns (transitions from 0 to 1) in the ordered sequence
+                A = 0;
+                for k = 2:size(P,2)-1
+                    if P(k) == 0 && P(k+1)==1
+                        A = A+1;
+                    end
+                end
+                if (A==1)
+                    BW_Del(i,j)=0;
+                    changing = 1;
+                end
+            end
+        end
+    end
+    BW_Thinned = BW_Thinned.*BW_Del;  % the deletion must after all the pixels have been visited
+    % Step 2 
+    for i=2:rows-1
+        for j = 2:columns-1
+            P = [BW_Thinned(i,j) BW_Thinned(i-1,j) BW_Thinned(i-1,j+1) BW_Thinned(i,j+1) BW_Thinned(i+1,j+1) BW_Thinned(i+1,j) BW_Thinned(i+1,j-1) BW_Thinned(i,j-1) BW_Thinned(i-1,j-1) BW_Thinned(i-1,j)];
+            if (BW_Thinned(i,j) == 1 && sum(P(2:end-1))<=6 && sum(P(2:end-1)) >=2 && P(2)*P(4)*P(8)==0 && P(2)*P(6)*P(8)==0)   % conditions
+                A = 0;
+                for k = 2:size(P,2)-1
+                    if P(k) == 0 && P(k+1)==1
+                        A = A+1;
+                    end
+                end
+                if (A==1)
+                    BW_Del(i,j)=0;
+                    changing = 1;
+                end
+            end
+        end
+    end
+    BW_Thinned = BW_Thinned.*BW_Del;
+end%while
+
+figure
+subplot(1,2,1)
+imshow(BW_Original)
+subplot(1,2,2)
+imshow(BW_Thinned)
---
+++ thinning.py
@@ -0,0 +1,79 @@
+"""
+===========================
+@Author  : Linbo<linbo.me>
+@Version: 1.0    25/10/2014
+This is the implementation of the 
+Zhang-Suen Thinning Algorithm for skeletonization.
+===========================
+"""
+import matplotlib
+import matplotlib.pyplot as plt
+import skimage.io as io
+"load image data"
+Img_Original =  io.imread( './data/test1.bmp')      # Gray image, rgb images need pre-conversion
+
+"Convert gray images to binary images using Otsu's method"
+from skimage.filter import threshold_otsu
+Otsu_Threshold = threshold_otsu(Img_Original)   
+BW_Original = Img_Original < Otsu_Threshold    # must set object region as 1, background region as 0 !
+
+def neighbours(x,y,image):
+    "Return 8-neighbours of image point P1(x,y), in a clockwise order"
+    img = image
+    x_1, y_1, x1, y1 = x-1, y-1, x+1, y+1
+    return [ img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1],     # P2,P3,P4,P5
+                img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1] ]    # P6,P7,P8,P9
+
+def transitions(neighbours):
+    "No. of 0,1 patterns (transitions from 0 to 1) in the ordered sequence"
+    n = neighbours + neighbours[0:1]      # P2, P3, ... , P8, P9, P2
+    return sum( (n1, n2) == (0, 1) for n1, n2 in zip(n, n[1:]) )  # (P2,P3), (P3,P4), ... , (P8,P9), (P9,P2)
+
+def zhangSuen(image):
+    "the Zhang-Suen Thinning Algorithm"
+    Image_Thinned = image.copy()  # deepcopy to protect the original image
+    changing1 = changing2 = 1        #  the points to be removed (set as 0)
+    while changing1 or changing2:   #  iterates until no further changes occur in the image
+        # Step 1
+        changing1 = []
+        rows, columns = Image_Thinned.shape               # x for rows, y for columns
+        for x in range(1, rows - 1):                     # No. of  rows
+            for y in range(1, columns - 1):            # No. of columns
+                P2,P3,P4,P5,P6,P7,P8,P9 = n = neighbours(x, y, Image_Thinned)
+                if (Image_Thinned[x][y] == 1     and    # Condition 0: Point P1 in the object regions 
+                    2 <= sum(n) <= 6   and    # Condition 1: 2<= N(P1) <= 6
+                    transitions(n) == 1 and    # Condition 2: S(P1)=1  
+                    P2 * P4 * P6 == 0  and    # Condition 3   
+                    P4 * P6 * P8 == 0):         # Condition 4
+                    changing1.append((x,y))
+        for x, y in changing1: 
+            Image_Thinned[x][y] = 0
+        # Step 2
+        changing2 = []
+        for x in range(1, rows - 1):
+            for y in range(1, columns - 1):
+                P2,P3,P4,P5,P6,P7,P8,P9 = n = neighbours(x, y, Image_Thinned)
+                if (Image_Thinned[x][y] == 1   and        # Condition 0
+                    2 <= sum(n) <= 6  and       # Condition 1
+                    transitions(n) == 1 and      # Condition 2
+                    P2 * P4 * P8 == 0 and       # Condition 3
+                    P2 * P6 * P8 == 0):            # Condition 4
+                    changing2.append((x,y))    
+        for x, y in changing2: 
+            Image_Thinned[x][y] = 0
+    return Image_Thinned
+ 
+
+"Apply the algorithm on images"
+BW_Skeleton = zhangSuen(BW_Original)
+# BW_Skeleton = BW_Original
+"Display the results"
+fig, ax = plt.subplots(1, 2)
+ax1, ax2 = ax.ravel()
+ax1.imshow(BW_Original, cmap=plt.cm.gray)
+ax1.set_title('Original binary image')
+ax1.axis('off')
+ax2.imshow(BW_Skeleton, cmap=plt.cm.gray)
+ax2.set_title('Skeleton of the image')
+ax2.axis('off')
+plt.show()
